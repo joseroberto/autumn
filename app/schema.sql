3b1d137dc8297b908801dd2297771ffca88af6d0
Sergio <engsergiodias28@gmail.com>
2016-07-16 14:59:41 -0300

    inseridos dados com insert em origem, equipamento e configuração

--- app/schema.sql
+++ app/schema.sql
@@ -1,6 +1,6 @@
-drop table if exists roteiro_manobra;
 drop table if exists equipamento;
 drop table if exists unidade;
+drop table if exists roteiro_manobra;
 drop table if exists roteiro_manobra_item;
 drop table if exists roteiro_comando;
 drop table if exists usuario;
@@ -9,15 +9,8 @@ drop table if exists execucao_item;
 
 PRAGMA foreign_keys = ON;
 
-create table roteiro_manobra (
-  id integer primary key autoincrement,
-  origem text not null,
-  equipamento text not null,
-  configuracao text not null
-);
-
 create table equipamento (
-  id integer primary key autoincrement,
+  id integer primary key,
   codigo text not null,
   tipo integer
 );
@@ -26,6 +19,19 @@ create table unidade(
   id integer primary key autoincrement,
   codigo text not null
 );
+
+create table roteiro_manobra (
+  id integer primary key autoincrement,
+  id_origem integer not null,
+  id_equipamento integer not null,
+  configuracao text not null,
+  foreign key (id_equipamento) references equipamento(id),
+  foreign key (id_origem) references unidade(id)
+);
+
+
+
+
 
 create table roteiro_manobra_item(
   id integer primary key autoincrement,
@@ -66,5 +72,16 @@ create table execucao_item(
   id_execucao integer not null,
   hora_execucao text not null,
   foreign key (id_execucao) references execucao(id)
-)
+);
+
+insert into roteiro_manobra (origem, equipamento, configuracao) values ('JCD', '14C1','Disjuntor e chaves do C1 fechadas');
+insert into roteiro_manobra (origem, equipamento, configuracao) values ('JCD', '14T1','Disjuntor e chaves do T1 fechadas');
+insert into roteiro_manobra (origem, equipamento, configuracao) values ('CGD', '12T5','Disjuntor e chaves de T5 associadas fechadas');
+insert into roteiro_manobra (origem, equipamento, configuracao) values ('CGD', '13L1','Disjuntor e chaves do L1 associadas fechadas');
+insert into roteiro_manobra (origem, equipamento, configuracao) values ('CTM', '14F8','Disjuntor e chaves DO F8 associadas fechadas');
+insert into roteiro_manobra (origem, equipamento, configuracao) values ('CTM', '14C3','Disjuntor e chaves do C3 associadas fechadas');
+insert into roteiro_manobra (origem, equipamento, configuracao) values ('CTM', '14W1','Disjuntor e chaves associadas DO W1 fechadas');
+insert into roteiro_manobra (origem, equipamento, configuracao) values ('CTM', '14D1','Disjuntor e chaves associadas do D1 fechadas');
+insert into roteiro_manobra (origem, equipamento, configuracao) values ('CTM', '04B1','Eventos terminados em impares conectadas');
+
 
