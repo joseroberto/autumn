f2ce86a3c51bce9bbf3564306295b54015975f58
Jose Roberto <jose.roberto@dinamonetworks.com>
2016-07-16 12:22:52 -0300

    Script de banco

---
+++ autman/schema.sql
@@ -0,0 +1,64 @@
+drop table if exists entries;
+
+PRAGMA foreign_keys = ON;
+
+create table roteiro_manobra (
+  id integer primary key autoincrement,
+  origem text not null,
+  equipamento text not null,
+  configuracao text not null
+);
+
+create table equipamento (
+  id integer primary key autoincrement,
+  codigo text not null,
+  tipo integer
+);
+
+create table unidade(
+  id integer primary key autoincrement,
+  codigo text not null
+);
+
+create table roteiro_manobra_item(
+  id integer primary key autoincrement,
+  id_roteiro_manobra integer not null,
+  id_comando integer null,
+  descricao text not null,
+  id_unidade integer not null,
+  foreign key (id_comando) references comando(id),
+  foreign key (id_roteiro_manobra) references roteiro_manobra(id),
+  foreign key (id_unidade) references unidade(id)
+);
+
+create table roteiro_comando(
+  id integer primary key autoincrement,
+  id_roteiro_manobra_item integer not null,
+  id_equipamento integer not null,
+  comando not null,
+  foreign key (id_roteiro_manobra_item) references roteiro_manobra_item(id),
+  foreign key (id_equipamento) references equipamento(id)
+);
+
+create table usuario(
+  id integer primary key autoincrement,
+  usuario text not null
+);
+
+create table execucao(
+  id integer primary key autoincrement,
+  data date,
+  id_roteiro_manobra integer not null,
+  id_usuario integer,
+  foreign key (id_roteiro_manobra) references roteiro_manobra(id),
+  foreign key (id_usuario) references usuario(id)
+);
+
+create table execucao_item(
+  id integer primary key autoincrement,
+  id_execucao integer not null,
+  hora_execucao text not null,
+  foreign key (id_execucao) references execucao(id)
+)
+
+
